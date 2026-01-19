a2840d1480bd2ef10039c03a493121615c79041a
saranbaji <saranbaji@gmail.com>
2026-01-19 23:01:20 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <p className="text-3xl text-green-900">Namaste! Welcome to Netflix + GPT</p>
+    </>
+  )
+}
+
+export default App
